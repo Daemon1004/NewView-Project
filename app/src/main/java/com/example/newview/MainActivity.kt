ef1be732ae09f6dd5fe31b7957f6633c5e7f1444
Daemon <kirill.mihaylov.2002@list.ru>
2023-03-24 12:37:42 +0300

    Initial commit

---
+++ app/src/main/java/com/example/newview/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.newview
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var homeFragment: Fragment
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        homeFragment = if (intent.getStringExtra("Mode") == "B") { HomeFragmentB() } else { HomeFragmentV() }
+
+        loadFragment(homeFragment)
+        findViewById<BottomNavigationView>(R.id.nav).setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> {
+                    loadFragment(homeFragment)
+                    true
+                }
+                R.id.settings -> {
+                    loadFragment(SettingsFragment())
+                    true
+                }
+                else -> {
+                    false
+                }
+            }
+        }
+
+    }
+    private fun loadFragment (Fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, Fragment)
+        transaction.commit()
+    }
+}
---
+++ app/src/main/java/com/example/newview/PreActivity.kt
@@ -0,0 +1,34 @@
+package com.example.newview
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+
+
+class PreActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pre)
+
+        findViewById<Button>(R.id.mode1).setOnClickListener {
+
+            goToMain("V")
+
+        }
+
+        findViewById<Button>(R.id.mode2).setOnClickListener {
+
+            goToMain("B")
+
+        }
+
+    }
+    private fun goToMain (Arg: String)
+    {
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("Mode", Arg)
+        startActivity(intent)
+        finish()
+    }
+}
