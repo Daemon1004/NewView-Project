77a0283692cd0f843ae3196564ac716dd1d5479e
Daemon <kirill.mihaylov.2002@list.ru>
2023-04-19 14:03:35 +0300

    New page for password settings; Renamed dimensions; Changed text color for editText (hint)

--- app/src/main/java/com/example/newview/PasswordSettingsFragment.kt
+++ app/src/main/java/com/example/newview/PasswordSettingsFragment.kt
@@ -1,14 +1,10 @@
 package com.example.newview
 
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.provider.Settings
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -17,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [SettingsFragment.newInstance] factory method to
+ * Use the [PasswordSettingsFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SettingsFragment : Fragment() {
+class PasswordSettingsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -38,32 +34,7 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<Button>(R.id.PersonalDataSettings).setOnClickListener {
-            (activity as MainActivity?)!!.loadFragment(PersonalDataSettingsFragment())
-        }
-
-        view.findViewById<Button>(R.id.PasswordSettings).setOnClickListener {
-            (activity as MainActivity?)!!.loadFragment(PasswordSettingsFragment())
-        }
-
-        view.findViewById<Button>(R.id.NotificationSettings).setOnClickListener {
-            val intent = Intent()
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                intent.action = Settings.ACTION_APP_NOTIFICATION_SETTINGS
-                intent.putExtra(Settings.EXTRA_APP_PACKAGE, context!!.packageName)
-            } else {
-                intent.action = "android.settings.APP_NOTIFICATION_SETTINGS"
-                intent.putExtra("app_package", context!!.packageName)
-                intent.putExtra("app_uid", context!!.applicationInfo.uid)
-            }
-            context!!.startActivity(intent)
-        }
+        return inflater.inflate(R.layout.fragment_password_settings, container, false)
     }
 
     companion object {
@@ -73,12 +44,12 @@ class SettingsFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment SettingsFragment.
+         * @return A new instance of fragment PasswordSettingsFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            SettingsFragment().apply {
+            PasswordSettingsFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
