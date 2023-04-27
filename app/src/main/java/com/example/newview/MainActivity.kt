4d332788f9807918e5e6a8fe228032ea3e166773
Daemon <kirill.mihaylov.2002@list.ru>
2023-04-27 18:20:58 +0300

    Migrate to AndroidX

--- app/src/main/java/com/example/newview/MainActivity.kt
+++ app/src/main/java/com/example/newview/MainActivity.kt
@@ -6,9 +6,9 @@ import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.IBinder
-import android.support.design.widget.BottomNavigationView
-import android.support.v4.app.Fragment
-import android.support.v7.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
 import com.example.newview.services.NetService
 
 class MainActivity : AppCompatActivity() {
