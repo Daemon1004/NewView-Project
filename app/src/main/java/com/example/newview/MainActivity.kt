d8cec1be57a611b05964da805a73951ce9604841
Daemon <kirill.mihaylov.2002@list.ru>
2023-05-29 15:24:25 +0300

    Connected firebase

--- app/src/main/java/com/example/newview/MainActivity.kt
+++ app/src/main/java/com/example/newview/MainActivity.kt
@@ -1,34 +1,11 @@
 package com.example.newview
 
-import android.content.ComponentName
-import android.content.Context
-import android.content.Intent
-import android.content.ServiceConnection
 import android.os.Bundle
-import android.os.IBinder
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.fragment.app.Fragment
 import androidx.appcompat.app.AppCompatActivity
-import com.example.newview.services.NetService
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var mService: NetService
-    private var mBound: Boolean = false
-
-    private val connection = object : ServiceConnection {
-
-        override fun onServiceConnected(className: ComponentName, service: IBinder) {
-            val binder = service as NetService.LocalBinder
-            mService = binder.getService()
-            mBound = true
-        }
-
-        override fun onServiceDisconnected(arg0: ComponentName) {
-            mBound = false
-        }
-    }
-
     private lateinit var homeFragment: Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -60,18 +37,4 @@ class MainActivity : AppCompatActivity() {
         transaction.replace(R.id.container, Fragment)
         transaction.commit()
     }
-
-    override fun onStart() {
-        super.onStart()
-        Intent(this, NetService::class.java).also { intent ->
-            bindService(intent, connection, Context.BIND_AUTO_CREATE)
-        }
-    }
-
-    override fun onStop() {
-        super.onStop()
-        unbindService(connection)
-        mBound = false
-    }
-
 }
