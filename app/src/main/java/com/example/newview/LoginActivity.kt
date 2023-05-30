6d51ab5dec7398fd877e67ddb92bddd5cdd4ec5b
Daemon <kirill.mihaylov.2002@list.ru>
2023-05-30 14:21:37 +0300

    Google sign in

--- app/src/main/java/com/example/newview/LoginActivity.kt
+++ app/src/main/java/com/example/newview/LoginActivity.kt
@@ -1,21 +1,79 @@
 package com.example.newview
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var launcher: ActivityResultLauncher<Intent>
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
-        findViewById<Button>(R.id.GoogleLoginButton).setOnClickListener {
+        auth = Firebase.auth
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            val task = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+            try {
 
-            startActivity(Intent(this, PreActivity::class.java))
-            finish()
+                val account = task.getResult(ApiException::class.java)
+                if (account != null)
+                {
+                    firebaseAuthWithGoogle(account.idToken!!)
+                }
 
+            } catch (e: ApiException) {
+                Log.d("SignIn", "Api exception")
+            }
         }
 
+        findViewById<Button>(R.id.GoogleLoginButton).setOnClickListener { signInWithGoogle() }
+
+    }
+
+    private fun getClient(): GoogleSignInClient {
+        val gso = GoogleSignInOptions
+            .Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        return GoogleSignIn.getClient(this, gso)
+    }
+    private fun signInWithGoogle() {
+        val signInClient = getClient()
+        launcher.launch(signInClient.signInIntent)
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential).addOnCompleteListener {
+            if (it.isSuccessful) {
+
+                Log.d("SignIn", "Signed")
+
+                /*
+                startActivity(Intent(this, PreActivity::class.java))
+                finish()
+                */
+
+            } else {
+
+                Log.d("SignIn", "Error")
+
+            }
+        }
     }
 }
