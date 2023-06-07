92f4426ced470439552ffdad2f2f8ad0dde2a040
Daemon <kirill.mihaylov.2002@list.ru>
2023-06-08 00:14:14 +0300

    Change theme fix

--- app/src/main/java/com/example/newview/HomeFragmentV.kt
+++ app/src/main/java/com/example/newview/HomeFragmentV.kt
@@ -23,7 +23,6 @@ class HomeFragmentV : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home_v, container, false)
     }
 
